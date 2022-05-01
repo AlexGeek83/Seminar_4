be06fa3d924e1f3c66baa1e9d6715a2ee98aa7ac
Alex <lrl.msk@yandex.ru>
2022-05-01 23:08:18 +0300

    add дз задача 27

--- ДЗ/Задача27/Program.cs
+++ ДЗ/Задача27/Program.cs
@@ -3,8 +3,19 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.Write("Введите число: ");
+string enterNumA = Console.ReadLine();
+int numA = Convert.ToInt32(enterNumA);
+int SumA = 0;
+// if(numA < 0)
+// numA = - numA;
 
-
-
+while (numA > 0)
+{
+    int numB = numA % 10;
+    SumA += numB;
+    numA /= 10;
+}
+System.Console.Write($"Сумма цифр введённого числа  равна {SumA}");
 
 
