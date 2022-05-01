9bfa48e3a20fb5a728e6b1435ae4900ea92a75e8
Alex <lrl.msk@yandex.ru>
2022-05-01 13:12:19 +0300

    add задача4

--- Задача4/Program.cs
+++ Задача4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+
+int [] array = new int [8];
+
+for (int i = 0; i < array.Length; i++)  // Задаем значание
+{
+    array[i] = new Random().Next(0,2);
+}
+for (int i = 0; i < array.Length; i++)  // Выводим значение
+{
+    System.Console.Write($"{array[i]} ");
+}
+
+
+    
+
+
