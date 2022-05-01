14d18867a61e8ee3e74eb87a7be1ecacf5f93f5e
Alex <lrl.msk@yandex.ru>
2022-05-01 12:21:54 +0300

    add задача3

---
+++ задача3/Program.cs
@@ -0,0 +1,22 @@
+﻿// программа, которая принимает на вход число N
+// и выдает произведение чисел от 1 до N - Факториал
+
+int Factorial(int N)
+{
+    int result = 1;
+    for (int i = 1; i <= N; i++)
+ {
+    result *= i;  // или result += i
+ }
+ return result;
+}
+ //int sum1;
+ //sum1 = int = Factorial(int N)
+ //System.Console.WriteLine(sum1);
+
+System.Console.WriteLine(Factorial(1));
+
+
+//Console.WriteLine($"Произведение чисел от 1 до {A} = {sum}");
+
+
