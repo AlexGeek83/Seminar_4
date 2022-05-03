e389ab7d4c66fb020a7bc4ab430c55b52f351e87
Alex <lrl.msk@yandex.ru>
2022-05-03 11:48:50 +0300

    add дз задача 29

--- ДЗ/Задача29/Program.cs
+++ ДЗ/Задача29/Program.cs
@@ -3,6 +3,21 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+// объявляем массив
+//int[] age;
+
+// выделяем память для массива
+//age = new int[5];
+
+int [] age = {1, 2, 5, 7, 19};
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i]} ");
+}
+PrintArray(age);
+
 
 
 
